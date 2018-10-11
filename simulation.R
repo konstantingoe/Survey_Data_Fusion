20ab1d702a3b250d661e730297c951e9ce18348b
Konstantin Göbler <konstantingobler@eduroam-078-104-001-130.wlan.univie.ac.at>
2018-10-11 18:52:41 +0200

    starting simulation

---
+++ simulation.R
@@ -0,0 +1,27 @@
+rm(list=ls())
+
+source("packages.R")
+source("functions.R")
+source(".path.R")
+
+soep <- import(paste(path, "soep_passive_ges.dta" , sep = "/"), setclass = "data.table")
+vskt.mp <- import(paste(path, "vskt_passiv_panel_ges.dta" , sep = "/"), setclass = "data.table")
+
+# perform simulation on the basis of soep
+# for this use soep twice delete in each set variables and keep X as common variables
+# then take data set A and randomly drop 2/3 of the data... then match the full dataset B with 
+# (X,Z) onto the smaller data set A with (X,Y).
+
+# todo: go into stata and reproduce rentenfile with income information as well...
+
+soepA <- select(soep, -education)
+B <- select(soep, -income)
+
+set.seed(1234)
+A <- sample_frac(soepA, 0.3, replace = F)
+
+
+
+
+
+
